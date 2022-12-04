1fe2c46e881047d15d3c3fbea685761dc207253a
Denny Setyawan Huda <dennshgit@gmail.com>
2022-12-04 21:15:34 +0700

    feat: get data with axios and render fact

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,22 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [fact, setFact] = useState("");
+
+  function generateCatFact() {
+    axios.get("https://catfact.ninja/fact").then((response) => {
+      setFact(response.data.fact);
+    });
+  }
+  useEffect(() => {
+    generateCatFact();
+  }, []);
   return (
     <div className="App">
-      <h1>hai</h1>
+      <h1>Cat Fact</h1>
+      <button onClick={generateCatFact}>Generate</button>
+      <p>{fact}</p>
     </div>
   );
 }
