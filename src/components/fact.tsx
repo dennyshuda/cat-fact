1f4d2299f98b52f78d37e388abc9045fdfe63070
Denny Setyawan Huda <dennshgit@gmail.com>
2022-12-05 12:50:10 +0700

    feat: fetching data with react query

---
+++ src/components/fact.tsx
@@ -0,0 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+export function Fact() {
+  async function getFacts() {
+    try {
+      const response = await axios.get("https://catfact.ninja/fact");
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const { data, isError, isLoading, refetch } = useQuery({
+    queryKey: ["catFact"],
+    queryFn: getFacts,
+  });
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error</span>;
+  }
+  return (
+    <div>
+      <h1>Random Cat Fact</h1>
+      <button onClick={() => refetch()}>Generate</button>
+      <h1>{data?.fact}</h1>
+      <p>{data?.length}</p>
+    </div>
+  );
+}
